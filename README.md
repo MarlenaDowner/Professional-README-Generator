27158f4252161a6b8f1650b9838a9ec7904d5b1b
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-02-24 22:41:45 +0000

    fixed gaps in table of contents

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-# README Generator
+# test
   ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
 
+   ### Description
 
-  
+   test
+
+   ### Table of Contents
+
+   * [Installation](#installation)
+
+   * [Usage](#usage)
+
+   * [License](#license)
+
+
+   * [Contributing](#contributing)
+
+   * [Test](#tests)
+
+   * [Questions](#questions)
+
+   ## Installation 
+
+   To install depensencies run the following command:
+
+   ```
+   npm i
+   ```
+
+   ## Usage
+
+   test
+
+
+
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,57 @@
 // function to generate markdown for README
-function renderLicenseBadge(license) {
+const renderLicenseBadge = (license) => {
   if (license !== "None") {
     return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
   }
   return''  
 }
 
+const renderLicenseLink = (License) => {
+  if(License !== "None") {
+    return `* [License](#license)\n`
+  }
+  return '';
+}
+
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
 
+   ### Description
 
-  
+   ${data.description}
+
+   ### Table of Contents
+
+   * [Installation](#installation)
+
+   * [Usage](#usage)
+
+   ${renderLicenseLink(data.license)}
+
+   * [Contributing](#contributing)
+
+   * [Test](#tests)
+
+   * [Questions](#questions)
+
+   ## Installation 
+
+   To install depensencies run the following command:
+
+   \`\`\`
+   ${data.installation}
+   \`\`\`
+
+   ## Usage
+
+   ${data.usage}
+
+
+
+
+
 `;
 }
 
